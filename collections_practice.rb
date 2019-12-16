3a18353fee3e691156afcafa684a84aec681cf5d
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-16 01:24:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
-
+def sort_array_asc (a= [25,7,1])
+  a.sort do |b,c|
+  b<=>c
+end 
+end 
+def sort_array_desc (a= ["dogs","cat","Horses"])
+  a.sort do |b,c|
+    c<=>b 
+  end 
+end 
+def sort_array_char_count()
