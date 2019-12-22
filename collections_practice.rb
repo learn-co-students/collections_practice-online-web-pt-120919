a8eb72a26ce7d0b6762dd675d7de5e5a796954d9
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 05:32:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
+require 'pry'
+
 def sort_array_asc(array)
   array.sort
 end
-
-sort_array_asc([25, 7, 1]) #[1,7,25]
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,28 +10,20 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) #[25, 14, 7]
-
 def sort_array_char_count(array)
   array.sort do |left, right|
     left.length <=> right.length
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) 
-
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
-
 def reverse_array(array)
   array.reverse
 end
-
-reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
 
 def kesha_maker(array)
   array.each do |item|
@@ -39,17 +31,16 @@ def kesha_maker(array)
   end
 end
 
-kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
-
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
-  
+  # using select method
+    # array.select do |word|
+    #   word[0] == "a"
+    # end
 end
-
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
 
 def sum_array(array)
   sum = 0
@@ -57,11 +48,18 @@ def sum_array(array)
     sum+=num
   end
   sum
-end 
-  
-end
 
-sum_array([11,4,7,8,9,100,134]) #273
+  # using reduce method
+    # array.reduce(:+)
+
+  # using inject method (short)
+     # array.inject(:+)
+
+  # using inject method (long)
+     # array.inject do |sum,x|
+     #  sum + x
+     # end
+end
 
 def add_s(array)
   array.collect do |word|
@@ -72,5 +70,3 @@ def add_s(array)
     end
   end
 end
-
-add_s(["hand","feet", "knee", "table"])
